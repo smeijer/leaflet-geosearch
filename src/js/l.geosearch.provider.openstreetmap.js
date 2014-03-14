ebabbdb58ad60a6eb720a3aea68f6f9471d66e2a
Rémi Duraffort <ivoire@videolan.org>
2014-03-14 09:32:59 +0100

    Fix url to GitHub project page

--- src/js/l.geosearch.provider.openstreetmap.js
+++ src/js/l.geosearch.provider.openstreetmap.js
@@ -1,7 +1,7 @@
 /**
  * L.Control.GeoSearch - search for an address and zoom to it's location
  * L.GeoSearch.Provider.OpenStreetMap uses openstreetmap geocoding service
- * https://github.com/smeijer/leaflet.control.geosearch
+ * https://github.com/smeijer/L.GeoSearch
  */
 
 L.GeoSearch.Provider.OpenStreetMap = L.Class.extend({
