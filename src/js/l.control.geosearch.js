e3fb78e55839c7e5b69167251afe812d589af15e
Andreas Heigl <andreas@heigl.org>
2014-05-09 13:44:25 +0200

    Retain zoomLevel when centering map
    This Commit introduces a way to retain the zoom-level of the map you are
    in. This retaining will of course only be used when the appropriate
    configuration is set.

--- src/js/l.control.geosearch.js
+++ src/js/l.control.geosearch.js
@@ -15,7 +15,8 @@ L.GeoSearch.Result = function (x, y, label) {
 L.Control.GeoSearch = L.Control.extend({
     options: {
         position: 'topcenter',
-        showMarker: true
+        showMarker: true,
+        retainZoomLevel: false
     },
 
     _config: {
@@ -183,7 +184,7 @@ L.Control.GeoSearch = L.Control.extend({
                 this._positionMarker.setLatLng([location.Y, location.X]);
         }
 
-        this._map.setView([location.Y, location.X], this._config.zoomLevel, false);
+        this._map.setView([location.Y, location.X], this._getZoomLevel(), false);
         this._map.fireEvent('geosearch_showlocation', {Location: location});
     },
 
@@ -208,5 +209,13 @@ L.Control.GeoSearch = L.Control.extend({
         } else if (e.keyCode === enter) {
             this.geosearch(queryBox.value);
         }
+    },
+
+    _getZoomLevel: function() {
+        if (! this.options.retainZoomLevel) {
+            return this._config.zoomLevel;
+        }
+        return this._map.zoom;
     }
+
 });
