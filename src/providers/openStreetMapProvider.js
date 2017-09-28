3c724d0362b6c9f3441ffd294bcff5bf1d1b02b7
Francis Lavoie <lavofr@gmail.com>
2017-09-28 18:07:07 -0400

    Fix #144

--- src/providers/openStreetMapProvider.js
+++ src/providers/openStreetMapProvider.js
@@ -13,6 +13,19 @@ export default class Provider extends BaseProvider {
     return `${protocol}//nominatim.openstreetmap.org/search?${paramString}`;
   }
 
+  endpointReverse({ data, protocol } = {}) {
+    const { params } = this.options;
+
+    const paramString = this.getParamString({
+      ...params,
+      format: 'json',
+      osm_id: data.raw.osm_id,
+      osm_type: this.translateOsmType(data.raw.osm_type),
+    });
+
+    return `${protocol}//nominatim.openstreetmap.org/reverse?${paramString}`;
+  }
+
   parse({ data }) {
     return data.map(r => ({
       x: r.lon,
@@ -24,5 +37,32 @@ export default class Provider extends BaseProvider {
       ],
       raw: r,
     }));
+  }
+
+  async search({ query, data }) {
+    // eslint-disable-next-line no-bitwise
+    const protocol = ~location.protocol.indexOf('http') ? location.protocol : 'https:';
+    if (data) {
+      const url = this.endpointReverse({ data, protocol });
+    } else {
+      const url = this.endpoint({ query, protocol });
+    }
+
+    const request = await fetch(url);
+    const json = await request.json();
+    return this.parse({ data: json });
+  }
+
+  translateOsmType(osm_type) {
+    if (osm_type === "node") {
+      return "N";
+    }
+    if (osm_type === "way") {
+      return "W";
+    }
+    if (osm_type === "relation") {
+      return "R";
+    }
+    return ""; // Unknown
   }
 }
