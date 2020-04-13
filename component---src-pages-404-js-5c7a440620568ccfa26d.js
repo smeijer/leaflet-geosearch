3215d1cde7205cc4b514ff064f9d9a331f6cb4db
Stephan Meijer <stephan.meijer@gmail.com>
2020-04-13 16:22:25 +0200

    update docs

---
+++ component---src-pages-404-js-5c7a440620568ccfa26d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{w2l6:function(t,e,n){"use strict";n.r(e);var i=n("aD51");e.default=function(){return Object(i.c)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
