1165814c38fd1cd9993349ea3f199182bf09c6af
Stephan Meijer <stephan.meijer@gmail.com>
2020-04-13 15:53:55 +0200

    export all providers

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ export { default as SearchElement } from './searchElement';
 export { default as BingProvider } from './providers/bingProvider';
 export { default as EsriProvider } from './providers/esriProvider';
 export { default as GoogleProvider } from './providers/googleProvider';
+export { default as LocationIQProvider } from './providers/locationIQProvider';
+export { default as OpenCageProvider } from './providers/openCageProvider';
 export { default as OpenStreetMapProvider } from './providers/openStreetMapProvider';
+
 export { default as Provider } from './providers/provider';
