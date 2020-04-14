f9787341e64fdff684c4535aa38dbaae7ddfa5d1
Stephan Meijer <stephan.meijer@gmail.com>
2020-04-14 12:45:20 +0200

    convert providers to typescript
    Added proper typing for both the api response as well as the returned result. But also added support for non string based queries. Most of the providers also accept query methods based on specific parameters instead of a generic string search.

--- src/providers/__tests__/openstreetmapProvider.spec.js
+++ src/providers/__tests__/openstreetmapProvider.spec.js
@@ -13,8 +13,8 @@ describe('OpenStreetMapProvider', function () {
     const result = results[0];
 
     expect(result.label).toBeTruthy();
-    expect(result.x).toEqual(fixtures[0].lon);
-    expect(result.y).toEqual(fixtures[0].lat);
+    expect(result.x).toEqual(+fixtures[0].lon);
+    expect(result.y).toEqual(+fixtures[0].lat);
     expect(result.bounds[0][0]).toBeGreaterThan(result.bounds[0][1]);
     expect(result.bounds[1][0]).toBeGreaterThan(result.bounds[1][1]);
     expect(result.bounds[0][0]).toBeLessThan(result.bounds[1][0]);
---
+++ src/providers/provider.ts
@@ -0,0 +1,84 @@
+export type PointTuple = [number, number];
+export type BoundsTuple = [PointTuple, PointTuple];
+
+export interface LatLng {
+  lat: number;
+  lng: number;
+}
+
+export interface SearchResult<TRawResult> {
+  x: number;
+  y: number;
+  label: string;
+  bounds: BoundsTuple;
+  raw: TRawResult;
+}
+
+export interface ProviderParams {
+  [key: string]: string | number | boolean;
+}
+
+export interface ProviderOptions {
+  params?: ProviderParams;
+}
+
+export enum RequestType {
+  SEARCH,
+  REVERSE,
+}
+
+export interface EndpointArgument {
+  query: string | { [key: string]: string | number | boolean };
+  type?: RequestType;
+}
+
+export interface SearchArgument {
+  query: string;
+}
+
+export interface ParseArgument<TData> {
+  data: TData;
+}
+
+export interface Provider<TRequestResult, TRawResult> {
+  options: ProviderOptions;
+
+  endpoint(options: EndpointArgument): string;
+  getParamString(params: ProviderParams): string;
+  parse(response: ParseArgument<TRequestResult>): SearchResult<TRawResult>[];
+  search(options: SearchArgument): Promise<SearchResult<TRawResult>[]>;
+}
+
+export default abstract class AbstractProvider<TRequestResult, TRawResult>
+  implements Provider<TRequestResult, TRawResult> {
+  options: ProviderOptions;
+
+  constructor(options: ProviderOptions = {}) {
+    this.options = options;
+  }
+
+  abstract endpoint(options: EndpointArgument): string;
+  abstract parse(response: ParseArgument<TRequestResult>): SearchResult<TRawResult>[];
+
+  getParamString(params: ProviderParams = {}): string {
+    const set = { ...this.options.params, ...params };
+    return Object.keys(set)
+      .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(set[key])}`)
+      .join('&');
+  }
+
+  getUrl(url: string, params?: ProviderParams): string {
+    return `${url}?${this.getParamString(params)}`;
+  }
+
+  async search(options: SearchArgument): Promise<SearchResult<TRawResult>[]> {
+    const url = this.endpoint({
+      query: options.query,
+      type: RequestType.SEARCH,
+    });
+
+    const request = await fetch(url);
+    const json: TRequestResult = await request.json();
+    return this.parse({ data: json });
+  }
+}
