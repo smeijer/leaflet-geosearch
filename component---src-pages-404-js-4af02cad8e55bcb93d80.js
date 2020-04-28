96988101bad99703ea49a34f80754a07b6d081ad
Stephan Meijer <stephan.meijer@gmail.com>
2020-04-28 09:50:09 +0200

    update docs

---
+++ component---src-pages-404-js-4af02cad8e55bcb93d80.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{w2l6:function(t,e,n){"use strict";n.r(e);var i=n("aD51");e.default=function(){return Object(i.c)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-4af02cad8e55bcb93d80.js.map
