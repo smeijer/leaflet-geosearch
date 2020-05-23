3eb5a1558bab1fde3b7bbcc032394c8280d2dbf2
Stephan Meijer <stephan.meijer@gmail.com>
2020-05-23 13:18:55 +0200

    update docs

--- component---src-pages-404-js-5c7a440620568ccfa26d.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{w2l6:function(t,e,n){"use strict";n.r(e);var i=n("aD51");e.default=function(){return Object(i.c)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
-//# sourceMappingURL=component---src-pages-404-js-5c7a440620568ccfa26d.js.map
--- styles-9af514da4117be1e676f.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[14],[]]);
-//# sourceMappingURL=styles-9af514da4117be1e676f.js.map
