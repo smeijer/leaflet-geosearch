306253065708c23e8088058ec0dc387449e27e4f
missinglink <insomnia@rcpt.at>
2021-10-12 10:12:49 +0200

    update docs

---
+++ component---src-pages-404-js-6c30f9cf322b6bcf6d22.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{w2l6:function(t,e,n){"use strict";n.r(e);var i=n("aD51");e.default=function(){return Object(i.c)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-6c30f9cf322b6bcf6d22.js.map
