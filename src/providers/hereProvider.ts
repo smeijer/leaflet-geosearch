35aa98c5ae54240e76f529bbfd34bc254282b9df
Tristan Lecorgne <teriblus@gmail.com>
2021-11-05 11:23:53 +0100

    here maps : replace geocode by autosuggest api

--- src/providers/hereProvider.ts
+++ src/providers/hereProvider.ts
@@ -2,8 +2,8 @@ import AbstractProvider, {
   EndpointArgument,
   LatLng,
   ParseArgument,
-  SearchResult,
-} from './provider';
+  SearchResult
+} from "./provider";
 
 export interface RequestResult {
   items: RawResult[];
@@ -37,24 +37,24 @@ export interface RawResult {
   };
 }
 
-export default class HereProvider extends AbstractProvider<
-  RequestResult,
-  RawResult
-> {
-  searchUrl = 'https://geocode.search.hereapi.com/v1/geocode';
+export default class HereProvider extends AbstractProvider<RequestResult,
+  RawResult> {
+  searchUrl = "https://geocode.search.hereapi.com/v1/autosuggest";
 
   endpoint({ query }: EndpointArgument): string {
-    const params = typeof query === 'string' ? { q: query } : query;
+    const params = typeof query === "string" ? { q: query } : query;
     return this.getUrl(this.searchUrl, params);
   }
 
   parse(response: ParseArgument<RequestResult>): SearchResult<RawResult>[] {
-    return response.data.items.map((r) => ({
-      x: r.position.lng,
-      y: r.position.lat,
-      label: r.address.label,
-      bounds: null,
-      raw: r,
-    }));
+    return response.data.items
+      .filter((r) => r.position !== undefined)
+      .map((r) => ({
+        x: r.position.lng,
+        y: r.position.lat,
+        label: r.address.label,
+        bounds: null,
+        raw: r
+      }));
   }
 }
