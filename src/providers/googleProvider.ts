a690a32685d1d24c5ed6ead78cbb4130cdcead91
Stephan Meijer <stephan.meijer@gmail.com>
2022-10-03 19:21:00 +0200

    chore: move loading of googlemaps to constructor

--- src/providers/googleProvider.ts
+++ src/providers/googleProvider.ts
@@ -26,11 +26,17 @@ export default class GoogleProvider extends AbstractProvider<
   RequestResult,
   google.maps.GeocoderResult
 > {
-  geocoder: () => Promise<google.maps.Geocoder>;
+  loader: Promise<google.maps.Geocoder>;
+  geocoder: google.maps.Geocoder | null = null;
 
   constructor(options: GoogleProviderOptions) {
     super(options);
-    this.geocoder = this.memoizeGeocoder(this.fetchGeocoder);
+
+    this.loader = new Loader(options).load().then((google) => {
+      const geocoder = new google.maps.Geocoder();
+      this.geocoder = geocoder;
+      return geocoder;
+    });
   }
 
   endpoint({ query }: EndpointArgument): never {
@@ -57,39 +63,15 @@ export default class GoogleProvider extends AbstractProvider<
     });
   }
 
-  memoizeGeocoder(method: () => Promise<google.maps.Geocoder>) {
-    let geocoder: Promise<google.maps.Geocoder>;
-
-    return async function (this: GoogleProvider) {
-      geocoder = geocoder || method.apply(this);
-      return geocoder;
-    };
-  }
-
-  async fetchGeocoder() {
-    return await new Loader(this.options as GoogleProviderOptions)
-      .load()
-      .then((google) => {
-        return new google.maps.Geocoder();
-      });
-  }
-
   async search(
     options: SearchArgument,
   ): Promise<SearchResult<google.maps.GeocoderResult>[]> {
-    const geoCoder = await this.geocoder();
-    const response = await geoCoder
-      .geocode(
-        {
-          address: options.query,
-        },
-        (
-          response: google.maps.GeocoderResult[] | null,
-          _: google.maps.GeocoderStatus,
-        ) => {
-          return { results: response };
-        },
-      )
+    const geocoder = this.geocoder || (await this.loader);
+
+    const response = await geocoder
+      .geocode({ address: options.query }, (response) => ({
+        results: response,
+      }))
       .catch((e: GeocodeError) => {
         if (e.code !== 'ZERO_RESULTS') {
           console.error(`${e.code}: ${e.message}`);
