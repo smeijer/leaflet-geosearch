5876c54c4cc7e5b8fbf34edaf0ee4992f3966f86
apl <admin@inkatlas.com>
2023-06-10 16:43:36 +0800

    handling <lat>, <lon> coordinate queries

---
+++ src/coords.ts
@@ -0,0 +1,21 @@
+// @ts-nocheck
+
+export function validateCoords(query) {
+    const q = query.query.trim();
+    const regex = /^(-?[0-9]*\.?\s*[0-9]*)\s*,?\s*(-?[0-9]*\.?[0-9]*)$/g;
+    var match = regex.exec(q);
+    if (match) {
+        var lat = Number(match[1]);
+        var lng = Number(match[2]);
+        if (( -90 < lat < 90) && ( -180 < lng < 180)) {
+            return [{
+                x: lng,
+                y: lat,
+                label: q,
+                bounds: null,
+                raw: {}
+            }];
+        }
+    }
+    return false;
+}
