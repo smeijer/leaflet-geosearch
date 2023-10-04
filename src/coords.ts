b54f341b8802e459b78bcb88db14e0434cf913c9
Stephan Meijer <stephan.meijer@gmail.com>
2023-10-04 23:44:06 +0200

    Apply suggestions from code review

--- src/coords.ts
+++ src/coords.ts
@@ -5,9 +5,9 @@ export function validateCoords(query) {
   const regex = /^(-?[0-9]*\.?\s*[0-9]*)\s*,?\s*(-?[0-9]*\.?[0-9]*)$/g;
   const match = regex.exec(q);
   if (match) {
-    let lat = Number(match[1]);
-    let lng = Number(match[2]);
-        if (( -90 < lat < 90) && ( -180 < lng < 180)) {
+    const lat = Number(match[1]);
+    const lng = Number(match[2]);
+    if (-90 < lat < 90 && -180 < lng < 180) {
             return [{
                 x: lng,
                 y: lat,
