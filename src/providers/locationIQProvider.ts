cb2d2d4510cda9ac8eb1b7738ef8dc1b79b497bf
Tim Hirzel <tim@growdown.com>
2023-11-13 23:22:41 -0500

    Update locationIQProvider.ts
    handle 404 errors from locationIQ

--- src/providers/locationIQProvider.ts
+++ src/providers/locationIQProvider.ts
@@ -1,6 +1,13 @@
 import OpenStreetMapProvider, {
   OpenStreetMapProviderOptions,
+  RawResult,
+  RequestResult,
 } from './openStreetMapProvider';
+
+import {
+  ParseArgument,
+  SearchResult,
+} from './provider';
 
 export default class LocationIQProvider extends OpenStreetMapProvider {
   constructor(options: OpenStreetMapProviderOptions) {
@@ -9,5 +16,12 @@ export default class LocationIQProvider extends OpenStreetMapProvider {
       searchUrl: `https://locationiq.org/v1/search.php`,
       reverseUrl: `https://locationiq.org/v1/reverse.php`,
     });
+  }
+
+  parse(response: ParseArgument<RequestResult>): SearchResult<RawResult>[] {
+    if (response.data.error) {
+      return [];
+    }
+    return super.parse(response);
   }
 }
