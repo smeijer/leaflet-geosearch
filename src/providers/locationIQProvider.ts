5f8c17282ed26818de4b05b06aa36b19c0bd3b5d
Tim Hirzel <tim@growdown.com>
2023-11-18 10:50:17 -0500

    fix: ensure remote errors in LocationIQ Provider are handled (#387)
    Co-authored-by: Stephan Meijer <stephan.meijer@gmail.com>

--- src/providers/locationIQProvider.ts
+++ src/providers/locationIQProvider.ts
@@ -1,6 +1,13 @@
 import OpenStreetMapProvider, {
   OpenStreetMapProviderOptions,
+  RawResult,
+  RequestResult,
 } from './openStreetMapProvider';
+
+interface RequestResultWithError extends RequestResult {
+  error?: string;
+}
+import { ParseArgument, SearchResult } from './provider';
 
 export default class LocationIQProvider extends OpenStreetMapProvider {
   constructor(options: OpenStreetMapProviderOptions) {
@@ -9,5 +16,14 @@ export default class LocationIQProvider extends OpenStreetMapProvider {
       searchUrl: `https://locationiq.org/v1/search.php`,
       reverseUrl: `https://locationiq.org/v1/reverse.php`,
     });
+  }
+
+  parse(
+    response: ParseArgument<RequestResultWithError>,
+  ): SearchResult<RawResult>[] {
+    if (response.data.error) {
+      return [];
+    }
+    return super.parse(response);
   }
 }
