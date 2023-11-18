18d1877fd6867e78563167e2136e626f993177c2
David Barina <dabler@gmail.com>
2023-11-18 16:49:09 +0100

    feat: add provider for https://civildefense.fit.vutbr.cz/search (#386)

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -7,6 +7,7 @@ export { default as LegacyGoogleProvider } from './legacyGoogleProvider';
 export { default as LocationIQProvider } from './locationIQProvider';
 export { default as OpenCageProvider } from './openCageProvider';
 export { default as OpenStreetMapProvider } from './openStreetMapProvider';
+export { default as CivilDefenseMapProvider } from './civilDefenseMapProvider';
 export { default as PeliasProvider } from './peliasProvider';
 export { default as MapBoxProvider } from './mapBoxProvider';
 export { default as GeoApiFrProvider } from './geoApiFrProvider';
