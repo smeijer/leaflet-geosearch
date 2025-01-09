4a97b4b5ebf3ab3cc6d793e25e08d644ef49aae8
Ramazan Sancar <ramazansancar4545@gmail.com>
2025-01-09 14:00:56 +0000

    upd: Sort provider list. A-Z fix: Fixed according to eslint rules.

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,15 +1,16 @@
 export { default as BingProvider } from './bingProvider';
+export { default as CivilDefenseMapProvider } from './civilDefenseMapProvider';
 export { default as EsriProvider } from './esriProvider';
+export { default as GeoApiFrProvider } from './geoApiFrProvider';
+export { default as GeoapifyProvider } from './geoapifyProvider';
 export { default as GeocodeEarthProvider } from './geocodeEarthProvider';
 export { default as GoogleProvider } from './googleProvider';
 export { default as LegacyGoogleProvider } from './legacyGoogleProvider';
+export { default as HereProvider } from './hereProvider';
 export { default as LocationIQProvider } from './locationIQProvider';
+export { default as MapBoxProvider } from './mapBoxProvider';
 export { default as OpenCageProvider } from './openCageProvider';
 export { default as OpenStreetMapProvider } from './openStreetMapProvider';
-export { default as CivilDefenseMapProvider } from './civilDefenseMapProvider';
 export { default as PeliasProvider } from './peliasProvider';
-export { default as MapBoxProvider } from './mapBoxProvider';
-export { default as GeoApiFrProvider } from './geoApiFrProvider';
-export { default as GeoapifyProvider } from './geoapifyProvider';
 
 export { default as Provider } from './provider';
