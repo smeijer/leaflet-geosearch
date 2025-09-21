f3de8ec17fef6b567bd1e829e66970ce761f5efb
Stephan Meijer <stephan.meijer@gmail.com>
2025-09-21 22:48:31 +0200

    docs: copy cname after build

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,43 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
+const fs = require('fs');
+
+const statics = ['CNAME'];
+
+function findRepoRoot(dir = process.cwd()) {
+  const gitDir = path.join(dir, '.git');
+
+  if (fs.existsSync(gitDir)) {
+    return dir;
+  }
+
+  const parentDir = path.dirname(dir);
+  if (parentDir === dir) {
+    return null;
+  }
+
+  return findRepoRoot(parentDir);
+}
+
+const root = findRepoRoot();
+const dist = path.join(root, '.docz/dist');
+
+exports.onPostBuild = ({ reporter }) => {
+  fs.mkdirSync(dist, { recursive: true });
+
+  for (const file of statics) {
+    const src = path.resolve(root, file);
+    const dest = path.resolve(dist, file);
+
+    if (!fs.existsSync(src)) {
+      reporter.info(`File ${file} not found`);
+      continue;
+    }
+
+    fs.copyFileSync(src, dest);
+    reporter.info(`Copied ${src} → ${dest}`);
+  }
+};
 
 exports.onCreateWebpackConfig = ({ actions, stage, loaders }) => {
   if (stage === 'build-html' || stage === 'develop-html') {
